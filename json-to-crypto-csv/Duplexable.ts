50c360d3a412a167e4b8b6e75fa27c49cd24b8c6
phil395 <philiplgvn@gmail.com>
2023-02-06 21:26:21 +0300

    add Duplexable class

---
+++ json-to-crypto-csv/Duplexable.ts
@@ -0,0 +1,29 @@
+import { Duplex } from "node:stream";
+
+export class Duplexable {
+	private data: string = '';
+
+	protected stream = new Duplex({
+		defaultEncoding: 'utf8',
+		write: (chunk, _, next) => {
+			this.data += chunk;
+			next();
+		},
+		read() { }
+	});
+
+	constructor() {
+		this.stream.on('finish', () => this.onReceiveData());
+		this.handleError();
+	}
+
+	private handleError(): void {
+		this.stream.on('error', (err) => {
+			console.log(err);
+		});
+	}
+
+	protected onReceiveData() {
+		// will be overridden
+	}
+}
