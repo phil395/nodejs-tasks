e626913725b1e8965c0032a68d76f71dd444601f
phil395 <philiplgvn@gmail.com>
2023-02-12 16:15:12 +0300

    fill main func

--- json-to-crypto-csv/index.ts
+++ json-to-crypto-csv/index.ts
@@ -1,25 +1,56 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import { pipeline } from 'node:stream'
+import { createGzip } from 'node:zlib'
 
 import { Converter } from './Converter'
+import { Cipher } from './Cipher'
+import { Inquirer, IQuestion } from './Inquirer'
+import { stain } from './utils'
 
+const questions: IQuestion[] = [
+	{ key: 'password', question: 'Input password' },
+	{ key: 'salt', question: 'Input salt' },
+	{ key: 'iv', question: 'Input iv' }
+]
 
-const main = () => {
-	const inputFilePath = path.resolve('data', 'comments.json');
-	const inputStream= fs.createReadStream(inputFilePath)
+const main = async () => {
+	const inputPath = path.resolve(__dirname, 'data', 'comments.json');
+	const outputPath = path.resolve(__dirname, 'data', 'output.txt.gz')
 
-	const csvStream = new Converter().getStream()
+	const answers = await new Inquirer<['password', 'salt', 'iv']>(questions).getAnswers()
+
+	const cipherStream = new Cipher(
+		"aes-192-cbc", 
+		answers.password,
+		answers.salt, 
+		answers.iv // "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16"
+	).getStream()
+
+	const converterStream = new Converter().getStream()
+
+	const onEnd = (err: NodeJS.ErrnoException | null) => {
+		if (err) {
+			console.log(err)
+			process.exit(1)
+		}
+		console.log(
+			stain("The file conversion was successful\n", "green"), 
+			"The result has been encrypted and gzipped\n",
+			"Output path: ", outputPath
+		)
+		process.exit(0)
+	}
 
 	pipeline(
-		inputStream,
-		csvStream,
-		(err) => {
-			if (err) return;
-		}
+		fs.createReadStream(inputPath),
+		converterStream,	
+		cipherStream,
+		createGzip(),
+		fs.createWriteStream(outputPath),
+		onEnd
 	)
-
 }
 
-
 main()
+
