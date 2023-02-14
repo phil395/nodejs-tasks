827a4d94614bef77fc60ba74edaf18d8f8674cdb
phil395 <philiplgvn@gmail.com>
2023-02-14 22:55:27 +0300

    add help

---
+++ json-to-crypto-csv/help.ts
@@ -0,0 +1,13 @@
+export const HELP = `
+   
+   Options: 
+     -h    Show this help.
+     -i    Specify path to intup file.
+     -n    Specify name of output file.
+
+   Notes:
+     Input file path required.
+     Format iv: 16 digits from 0 to 255. Digits must be separated by one space.
+     Example iv: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+ 
+`
